8845a2c3ca5a711de87f93a5107359859131b481
Nadia Naveed <naveed.riay@gmail.com>
2020-04-07 18:03:38 +0100

    Pushed my latest work to git

--- tests/test-page.js
+++ tests/test-page.js
@@ -2,7 +2,7 @@ import {Selector} from 'testcafe';
 
 class TestPage {
     checkBox    = Selector('#checkbox-example input');
-    RemoveBtn   = Selector('#checkbox-example button').withText('Remove');
+    RemoveBtn   = Selector('#checkbox-example button');
     AddBtn      = Selector('#checkbox-example button').withText('Add');
     CheckboxMsg = Selector('#checkbox-example p');
 
